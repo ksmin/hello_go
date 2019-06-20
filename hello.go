4751ac6838d59aa6682694c885f13b43d06ade87
KANG SANG MIN <ksmini82@gmail.com>
2019-06-20 11:29:56 +0900

    Added make and array examples.

--- hello.go
+++ hello.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+func Sum(arr *[3]float64) (sum float64) {
+	for idx, val := range *arr {
+		fmt.Println("idx:", idx, "val:", val)
+		sum += val
+	}
+	return sum
+}
+
 func main() {
-  fmt.Println("Hello, world.")
+	fmt.Println("Hello, world.")
+
+	// make example
+	sliceA := make([]int, 0, 3)
+
+	for i := 1; i <= 15; i++ {
+		sliceA = append(sliceA, i)
+		fmt.Println("sliceA len:", len(sliceA), "cap:", cap(sliceA))
+	}
+	fmt.Println("sliceA:", sliceA)
+
+	sliceB := []int{4, 5, 6}
+	sliceA = append(sliceA, sliceB...)
+	fmt.Println("sliceA:", sliceA)
+
+	target := make([]int, len(sliceA), cap(sliceA))
+	copy(target, sliceA)
+	fmt.Println("target:", target)
+
+	// Array example
+	array := [...]float64{7.0, 8.5, 9.1}
+	sum := Sum(&array)
+	fmt.Println("sum:", sum)
 }
